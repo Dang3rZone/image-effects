42501d61d899c86fcb6713894aea4a4448a011fb
Andres Rosales <andresrosalespinzon@gmail.com>
2022-05-20 16:43:36 +0200

    feat:reading pictures

--- public/main.js
+++ public/main.js
@@ -1,1 +1,19 @@
-console.log('test');
+function init() {
+  const input = document.getElementById('upload');
+  const fileReader = new FileReader();
+
+  fileReader.onload = (e) => {
+    let base64 = fileReader.result.replace(
+      /^data:image\/(png|jpg|jpeg);base64,/,
+      ''
+    );
+    console.log(base64);
+    console.log(input.files[0]);
+  };
+
+  input.addEventListener('change', () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+}
+
+init();
